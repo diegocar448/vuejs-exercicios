45dd3207d2fdd763eaae7f9b44b11edef5d4b7ef
diegocar448 <diegocar448@hotmail.com>
2018-10-10 15:09:50 -0300

    Components Vue CLI

---
+++ my-project2/src/components/SegundoComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <p>Segundo Teste</p>
+        <span>{{msg}}</span>
+        <test-component></test-component>
+    </div>
+</template>
+
+<script>
+import TestComponent from './TestComponent'
+
+export default{
+    data(){
+        return{
+            msg:"Segundo Component"
+        }
+    },
+    components:{
+        TestComponent
+    }
+}
+</script>
+
+<style scoped>
+p{color:green;}
+</style>
+
---
+++ my-project2/src/components/TestComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <!-- quando tiver mais de 1 item na tag principal precisa criar um segundo para agrupa-los -->
+        <p>Teste 2</p>
+        <span>{{msg}}</span>
+    </div>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            msg:'Teste de Vue JS 2'
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+p{color:red;}
+</style>
+
