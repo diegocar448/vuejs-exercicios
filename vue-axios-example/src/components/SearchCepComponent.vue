77e5221442af58639325715dd23a76c11d973cbc
diegocar448 <diegocar448@hotmail.com>
2018-10-11 11:18:41 -0300

    Teste 1 viaCep com VueJS

---
+++ vue-axios-example/src/components/SearchCepComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1 v-text="title"></h1>
+        <form @submit.prevent="onSubmit">
+            <input type="text" placeholder="CEP:" v-model="cep">
+            <button type="submit">Buscar</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            title:'Busca CEP',
+            cep:''
+        }
+    },
+    methods:{
+        onSubmit(){
+            alert(this.cep)
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
