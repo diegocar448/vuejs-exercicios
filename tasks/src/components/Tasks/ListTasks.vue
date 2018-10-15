335169d77b1d3afef17a5f01cc6769107379aee6
diegocar448 <diegocar448@hotmail.com>
2018-10-15 16:14:08 -0300

    Listagem

--- tasks/src/components/Tasks/ListTasks.vue
+++ tasks/src/components/Tasks/ListTasks.vue
@@ -1,6 +1,26 @@
 <template>
     <div>
-        <h2>{{ title }}</h2>
+        <h2 class="text-center">{{ title }}</h2>
+
+        <table class="table table-dark">
+            <thead>
+                <tr>
+                    <th>Id.</th>
+                    <th>Nome</th>
+                    <th width="180px">Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(task, index) in tasks" :key="index">
+                    <td>{{ task.id }}</td>
+                    <td>{{ task.name }}</td>
+                    <td>
+                        <a class="btn btn-info" href="">Editar</a>
+                        <a class="btn btn-danger"  href="">Deletar</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
@@ -8,7 +28,13 @@
 export default {
     data(){
         return{
-            title: 'Lista de Tarefas'
+            title: 'Lista de Tarefas',
+            tasks:[
+                {id:12, name:'nuke'},
+                {id:14, name:'hope'},
+                {id:16, name:'high'},
+                {id:18, name:'take'},
+            ]
         }
     }
 }
