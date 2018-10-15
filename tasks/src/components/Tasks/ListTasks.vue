43a1064e1dcde64584fc878a714f3071a2d02582
diegocar448 <diegocar448@hotmail.com>
2018-10-15 17:09:36 -0300

    gerar registros com vuejs

--- tasks/src/components/Tasks/ListTasks.vue
+++ tasks/src/components/Tasks/ListTasks.vue
@@ -2,6 +2,11 @@
     <div>
         <h2 class="text-center">{{ title }}</h2>
 
+        <form @submit.prevent="save" class="form form-inline">
+            <input class="form-control" type="text" placeholder="Nome Tarefa" v-model="task.name">
+            <button class="btn btn-primary" type="submit">Enviar</button>
+        </form>
+
         <table class="table table-dark">
             <thead>
                 <tr>
@@ -30,16 +35,41 @@ export default {
         return{
             title: 'Lista de Tarefas',
             tasks:[
-                {id:12, name:'nuke'},
+                /* {id:12, name:'nuke'},
                 {id:14, name:'hope'},
                 {id:16, name:'high'},
-                {id:18, name:'take'},
-            ]
+                {id:18, name:'take'}, */
+            ],
+            task:{
+                id:'',
+                name:''
+            }
+        }
+    },
+    methods:{
+        save(){
+            //gerar id de task
+            let idTask = this.tasks.length + 1
+
+            //agora vamos adicionar ao nosso task e aparece em nossa listagem
+            this.task.id = idTask
+
+            //alert(this.task.name)
+            //adicionar o novo registro a tabela
+            this.tasks.push(this.task)
+            //limpar o campo input do form
+            this.task = {
+                id:'',
+                name:''
+            }
+            
         }
     }
 }
 </script>
 
 <style scoped>
-
+form{
+    margin:20px 0px
+}
 </style>
