33bc324b55b52d7a7fd37290cefc622de3c67676
diegocar448 <diegocar448@hotmail.com>
2018-10-15 15:59:08 -0300

    adicionando recursos do bootstrap ao vuejs

--- tasks/src/main.js
+++ tasks/src/main.js
@@ -1,5 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+//importar o boostrap para o vuejs
+import BootstrapVue from 'bootstrap-vue'
+//importar as bibliotecas css
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+//adicionando o bootstrap ao vuejs
+Vue.use(BootstrapVue);
 
 new Vue({
   el: '#app',
