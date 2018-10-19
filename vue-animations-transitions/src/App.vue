8dab715b5f1c1e1a9f656f9ab7c68cbd5452cb29
diegocar448 <diegocar448@hotmail.com>
2018-10-19 14:31:30 -0300

    quiz vuejs

--- vue-animations-transitions/src/App.vue
+++ vue-animations-transitions/src/App.vue
@@ -11,7 +11,7 @@
     </div>
 </template>
 
-<script>
+<script> 
     import Quiz from './components/Animations/Quiz'
 
     export default {
--- vue-animations-transitions/src/components/Animations/Question.vue
+++ vue-animations-transitions/src/components/Animations/Question.vue
@@ -1,15 +1,47 @@
 <template>
     <div>
         <h2 v-text="question"></h2>
+
+        <form @submit.prevent="checkResult">
+            <input type="text" placeholder="Resposta:" v-model="reply">
+            <button type="submit">Responder</button>
+        </form>
     </div>
 </template>
 
 <script>
 export default {
     data(){
-        return{
-            title: 'Quiz',
-            question: 'Gerando pergunta...'
+        return{            
+            question: 'Gerando pergunta...',
+            result:0,
+            reply:''
+        }
+    },
+    created(){
+        this.generateQuestion()
+    },
+    methods:{
+        generateQuestion(){
+            let numberOne = this.generateRandomNumber(1, 100) 
+            let numberTwo = this.generateRandomNumber(1, 100) 
+
+            this.result = numberOne + numberTwo
+
+            this.question =  `Qual é o resutado da soma entre ${numberOne} + ${numberTwo} ? `
+        },
+        //função para gerar um numero randomico entre o min e o max
+        //round é para retornar numeros inteiros 
+        //random gerará números aleatórios
+        generateRandomNumber(min, max){
+            return Math.round(Math.random() * (max - min) + min)
+        },
+        //aqui verifica se o resultado que o usario informou esta correto
+        checkResult(){
+            if(this.reply == this.result)
+                alert("Acertou!!!")
+            else
+                alert("Errou!!!")
         }
     }
 }
