d4220d7f8bdb3e18fa6c698360367656adc2403b
Diego <diegocar448@hotmail.com>
2018-10-21 18:36:28 -0300

    vue $http https://github.com/pagekit/vue-resource/blob/develop/docs/config.md

---
+++ vue-resource-project/src/components/SearchCepComponent.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h1 v-text="title"></h1>
+
+        <form @submit.prevent="onSubmit">
+            <input type="text" placeholder="Informe o CEP:" v-model="cep">
+            <button type="submit">Buscar CEP</button>
+        </form>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            title: 'Busca Cep com Vue JS',
+            cep:''            
+        }
+    },
+    methods:{
+        onSubmit(){
+            //alert(this.cep)
+            this.$http.get(`https://api.postmon.com.br/v1/cep/${this.cep}`)
+                    .then(response => {
+                        console.log(response)
+                    }, error => console.log(error))
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
