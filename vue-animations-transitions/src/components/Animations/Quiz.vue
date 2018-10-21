c88a3356b1f03e42d25ed8429eea432ebf6ceb75
Diego <diegocar448@hotmail.com>
2018-10-21 17:05:13 -0300

    transform rotate vuejs

--- vue-animations-transitions/src/components/Animations/Quiz.vue
+++ vue-animations-transitions/src/components/Animations/Quiz.vue
@@ -5,10 +5,19 @@
         <!-- <app-question @changeMode="changeMode"></app-question> -->
         <!-- pegamos o component pegando ele pelo alias da
             propriedade e colocando no :is -->
-        <component            
+        <!-- <component            
             :is="mode"
             @changeMode="changeMode">
-        </component>
+        </component> -->
+
+
+        <!-- out-in : só quando ele sair vai aplicar a entrada || in-out somente quando ele finalizar a entrada ele roda a saída -->
+        <transition name="effect" mode="out-in">
+            <component            
+                :is="mode"
+                @changeMode="changeMode">
+            </component>
+        </transition>
     </div>
 </template>
 
@@ -45,5 +54,34 @@ export default {
 </script>
 
 <style scoped>
+    /* -active é enquanto acontece o efeito */
+    .effect-leave-active{
+        /* efeito durante a saida do componente*/
+        animation: effect-out 2.5s;
+    }
+    .effect-enter-active{
+        /* efeito durante a entrada do componente*/
+        animation: effect-in 2.5s;
+    }
+
+    @keyframes effect-out {
+        /*"from" é para onde estou indo e "on" é onde estou*/
+
+        /*transform é para rotacionar */
+        /* 0 deg == zero graus */
+        from{
+            transform: rotateY(0deg);
+        }to{
+            transform:rotateY(90deg);
+        }
+    }
+
+    @keyframes effect-in{
+        from{
+            transform:rotateX(90deg);
+        }to{
+            transform:rotateX(0deg);
+        }
+    }
 
 </style>
