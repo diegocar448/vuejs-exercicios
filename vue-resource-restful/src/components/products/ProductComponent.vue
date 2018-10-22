6b2b517cddd4e0f906a5730fa656ffad1f142fd5
diegocar448 <diegocar448@hotmail.com>
2018-10-22 13:48:08 -0300

    listar items da api restful

--- vue-resource-restful/src/components/products/ProductComponent.vue
+++ vue-resource-restful/src/components/products/ProductComponent.vue
@@ -1,6 +1,28 @@
 <template>
     <div>
         <h1>{{ title }}</h1>
+
+        <table class="table table-dark">
+            <thead>
+                <tr>
+                    <th>#id</th>
+                    <th>Nome</th>
+                    <th>Descrição</th>
+                    <th width="200">Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(product, index) in products.data" :key="index">
+                    <td>{{product.id}}</td>
+                    <td>{{product.name}}</td>
+                    <td>{{product.description}}</td>
+                    <td>
+                        <button class="btn btn-sm btn-success">Editar</button>
+                        <button class="btn btn-sm btn-danger">Apagar</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
@@ -8,7 +30,22 @@
 export default {
     data(){
         return {
-            title:'Lista de Produtos'
+            title:'Lista de Produtos',
+            products:{}
+        }
+    },
+    created(){
+        this.getProducts()
+    },    
+    methods:{
+        getProducts(){
+            this.$http.get('http://webservice-laravel-5-5.test/api/v1/products')
+                //then é para pegar o retorno
+                .then(response => {
+                    this.products = response.body
+                }, error =>{
+                    console.log(error)
+                })
         }
     }
 }
--- vue-resource-restful/src/main.js
+++ vue-resource-restful/src/main.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
-import BootstrapVue from 'bootstrap-vue'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
+import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 //importar o arquivo de rotas index.js
 import routes from './routes/index'
 
-Vue.use(BootstrapVue);
 Vue.use(VueRouter)
+Vue.use(VueResource);
+Vue.use(BootstrapVue);
 
 const router = new VueRouter({
   //mode:history para não aparecer a # hashtag na url
