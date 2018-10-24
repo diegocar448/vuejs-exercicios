4a00f5f1af65f64a735b7360877fa951582ab14c
diegocar448 <diegocar448@hotmail.com>
2018-10-24 15:04:02 -0300

    Começando a trabalhar com estados no Vuex

--- curso-vuex/src/components/tasks/TaskComponent.vue
+++ curso-vuex/src/components/tasks/TaskComponent.vue
@@ -1,14 +1,26 @@
 <template>
     <div>
         <h1 v-text="title"></h1>
+
+        <ul>
+            <li v-for="(task,index) in getTasks" :key="index">
+                {{ task.name }}
+            </li>
+        </ul>
     </div>
 </template>
+
 
 <script>
 export default {
     data(){
         return{
             title:'Lista de Tarefas'
+        }
+    },
+    computed:{
+        getTasks(){
+            return this.$store.state.tasks
         }
     }
 }
--- curso-vuex/src/main.js
+++ curso-vuex/src/main.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './routes/index'
-import Vuex from 'vuex'
-
-
-Vue.use(Vuex)
+import store from './store/index'
 
 
 
 new Vue({
   el: '#app', 
   router,
+  store,
   render: h => h(App)
 })
