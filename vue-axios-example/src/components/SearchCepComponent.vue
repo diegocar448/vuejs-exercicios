ee9dbe539594871478a7cf70d945f66f3eb07e3b
diegocar448 <diegocar448@hotmail.com>
2018-10-29 11:36:48 -0300

    axios request para viacep

--- vue-axios-example/src/components/SearchCepComponent.vue
+++ vue-axios-example/src/components/SearchCepComponent.vue
@@ -6,20 +6,48 @@
             <input type="text" placeholder="CEP:" v-model="cep">
             <button type="submit">Busca</button>
         </form>
+
+        <div v-if="address.cep">
+            <p>Cep: {{ address.cep }}</p>
+            <p>logradouro: {{ address.logradouro }}</p>
+            <p>complemento: {{ address.complemento }}</p>
+            <p>bairro: {{ address.bairro }}</p>
+            <p>localidade: {{ address.localidade }}</p>
+        </div>
     </div>
 </template>
 
 <script>
+//Para usar o axios nesse componente 
+//precisamos importa
+import axios from 'axios' 
+
+
 export default {
     data(){
         return{
             title:'Busca CEP',
-            cep:''
+            cep:'',
+            address: {}
         }
     },
     methods:{
         onSubmit(){
-            alert(this.cep)
+            //alert(this.cep)
+            axios.get(`https://viacep.com.br/ws/${this.cep}/json/`)
+                .then(response => {
+                    console.log(response.data)
+
+                    this.address = response.data
+
+                    
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+                .finally(() => console.log('Finally'))
+
+                
         }
     }
     
